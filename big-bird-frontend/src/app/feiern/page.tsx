bab57e56c9116a41467b3f95c9acfbd56fcc7f44
Carolin Mache <caromache2712@gmail.com>
2024-05-22 21:11:58 +0200

    Feierbilder zur Seite hinzugefügt

--- big-bird-frontend/src/app/feiern/page.tsx
+++ big-bird-frontend/src/app/feiern/page.tsx
@@ -8,6 +8,86 @@ export default function Datenschutz() {
                 <div className="text-center">
                     <p>Schreib uns gerne über das Kontaktformular an.</p>
                 </div>
+
+                <div className="row mt-5">
+                    <div className="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                        <img
+                            src="/images/feiern/feiern-01.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+
+                        <img
+                            src="/images/feiern/feiern-02.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+                        <img
+                            src="/images/feiern/feiern-03.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+                    </div>
+
+                    <div className="col-lg-3 mb-4 mb-lg-0">
+                        <img
+                            src="/images/feiern/feiern-04.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+
+                        <img
+                            src="/images/feiern/feiern-05.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+                        <img
+                            src="/images/feiern/feiern-06.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+                    </div>
+
+                    <div className="col-lg-3 mb-4 mb-lg-0">
+                        <img
+                            src="/images/feiern/feiern-07.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+
+                        <img
+                            src="/images/feiern/feiern-08.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+                        <img
+                            src="/images/feiern/feiern-09.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+                    </div>
+
+                    <div className="col-lg-3 mb-4 mb-lg-0">
+                        <img
+                            src="/images/feiern/feiern-10.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+
+                        <img
+                            src="/images/feiern/feiern-11.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+
+                        <img
+                            src="/images/feiern/feiern-12.jpg"
+                            className="w-100 shadow-1-strong mb-4"
+                            alt=""
+                        />
+                    </div>
+
+                </div>
             </div>
             <ContactSection pageName={'Feiern'} subjects={['sub1', 'sub2', 'sub3']}/>
         </>
