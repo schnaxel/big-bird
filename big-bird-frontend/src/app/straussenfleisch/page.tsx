e693c3606d13567d37ff6872ccc7cae890a49699
Carolin Mache <caromache2712@gmail.com>
2024-05-24 15:45:03 +0200

    Straußenfleischinformationen hinzugefügt

--- big-bird-frontend/src/app/straussenfleisch/page.tsx
+++ big-bird-frontend/src/app/straussenfleisch/page.tsx
@@ -1,14 +1,124 @@
 import ContactSection from "@/src/components/ContactSection/ContactSection";
-import Link from "next/link";
 
 export default function Straussenfleisch() {
     return (
         <>
 
             <div className={'container text-center'}>
-                <Link href="https://mocfor.com" className="btn btn-outline-primary">
-                    zum Straußenfleisch Shop
-                </Link>
+
+                <div className="mt-5">
+                    <h2>Unsere Produkte</h2>
+
+                    <div className="row my-5 text-center bg-primary bg-opacity-10">
+                        <div className="col-md-4 col-12 p-0">
+                            <img src="/images/placeholder/straußenfleisch-filet.jpg" alt={''} className="img-fluid w-100"></img>
+                        </div>
+                        <div className="col-md-8 col-12 text-start px-5 row justify-content-md-center my-md-auto mt-5">
+                            <h3>Straußenfilet</h3>
+                            <div className="mt-2">
+                                <p>
+                                    Das Straußenfilet zeichnet sich durch einen niedrigen Cholesterin- und 
+                                    Fettgehalt aus, ist jedoch reich an Eiweiß und Eisen, was es 
+                                    besonders gesund und leicht verdaulich macht.
+                                </p>
+                                <p>Das Straußenfilet wird zuerst scharf in der Pfanne angebraten und 
+                                    anschließend im Ofen saftig gegart. Sein Aroma ist nussig, der 
+                                    Geschmack auf der Zunge butterzart mit einer feinen Note von Wild 
+                                    und Leber.
+                                </p>
+                                <p>
+                                    Die hohe Qualität des eiweißreichen Straußenfilets erkennt man 
+                                    besonders an der geringen Fettmarmorierung.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row my-5 text-center bg-primary bg-opacity-10">
+                        <div className="col-md-4 col-12 p-0">
+                            <img src="/images/placeholder/straußenfleisch-steak.jpg" alt={''} className="img-fluid w-100"></img>
+                        </div>
+                        <div className="col-md-8 col-12 text-start px-5 row justify-content-md-center my-md-auto mt-5">
+                            <h3>Straußensteak</h3>
+                            <div className="mt-4">
+                                <p>
+                                    Unsere Steaks werden am besten wie ein Filet auf dem Grill 
+                                    zubereitet. Auch hier sind die Nährwerte wieder besonders beeindruckend: 
+                                    Dieses außergewöhnliche Geschmackserlebnis enthält erstaunlich wenig 
+                                    Cholesterin und Fett.
+                                </p>
+                                <p>
+                                    Gleichzeitig hat es sogar einen höheren Eiweißgehalt als das 
+                                    Fleisch von Huhn oder Pute. Deshalb ist gerade für Sportler und 
+                                    Diätbewusste ein saftiges Steak von unseren Big Birds genau das Richtige.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row my-5 text-center bg-primary bg-opacity-10">
+                        <div className="col-md-4 col-12 p-0">
+                            <img src="/images/placeholder/straußenfleisch-filet.jpg" alt={''} className="img-fluid w-100"></img>
+                        </div>
+                        <div className="col-md-8 col-12 text-start px-5 row justify-content-md-center my-md-auto mt-5">
+                            <h3>Straußenbraten</h3>
+                            <div className="mt-4">
+                                <p>
+                                    Das zarte Fleisch unserer Big Birds wird langsam gegart und 
+                                    entfaltet dabei einen einzigartigen Geschmack.
+                                </p>
+                                <p>
+                                    Dieser Braten verleiht euren Gerichten eine besondere Note und 
+                                    hebt sie auf ein neues kulinarisches Niveau.
+                                </p>
+                                <p>
+                                    Unser Braten zergeht auf der Zunge und verwöhnt den Gaumen.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row my-5 text-center bg-primary bg-opacity-10">
+                        <div className="col-md-4 col-12 p-0">
+                            <img src="/images/placeholder/straußenfleisch-steak.jpg" alt={''} className="img-fluid w-100"></img>
+                        </div>
+                        <div className="col-md-8 col-12 text-start px-5 row justify-content-md-center my-md-auto mt-5">
+                            <h3>Straußenwurst</h3>
+                            <div className="mt-4">
+                                <p>                                
+                                    Unsere Straußenbratwurst ist weich, zart und saftig und besteht 
+                                    hauptsächlich aus fettarmem Straußenfleisch.
+                                </p>
+                                <p>
+                                    Unsere Wurstspezialitäten sind keine Massenware, sondern entstehen in echter Handarbeit.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row my-5 text-center bg-primary bg-opacity-10">
+                        <div className="col-md-4 col-12 p-0">
+                            <img src="/images/placeholder/straußenfleisch-filet.jpg" alt={''} className="img-fluid w-100"></img>
+                        </div>
+                        <div className="col-md-8 col-12 text-start px-5 row justify-content-md-center my-md-auto mt-5">
+                            <h3>Burger Patties vom Strauß</h3>
+                            <div className="mt-4">
+                                <p>
+                                    Weniger ist mehr - das gilt besonders für Burger mit unserem Burger Patty vom Strauß.
+                                </p>
+                                <p>
+                                    Käse, Sauce und andere Zutaten sollten unserer Erfahrung nach 
+                                    eine untergeordnete Rolle spielen, wenn ihr eure Freunde beim 
+                                    nächsten Kochevent begeistern wollen.
+                                </p>
+                                <p>
+                                    Das fett- und cholesterinarme Patty aus Straßenfleisch spricht für sich.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="text-center my-5">
                     <p>
                         Unser Straußenfleisch schmeckt nicht nur sehr gut, sondern ist auch gesund!
@@ -26,7 +136,7 @@ export default function Straussenfleisch() {
 
                 <div className="row my-5 text-center">
                     <div className="col-md-6 col-12">
-                        <img src="/images/placeholder/straußenfleisch-steak.jpg" alt={''} className="img-fluid"></img>
+                        <img src="/images/placeholder/straußenfleisch-steak.jpg" alt={''} className="img-fluid w-75"></img>
                     </div>
                     <div className="col-md-6 col-12 row justify-content-md-center my-md-auto mt-5">
                         <h3>Unser Fleisch ist ideal für:</h3>
@@ -44,9 +154,9 @@ export default function Straussenfleisch() {
                 </div>
 
                 <div className="row mt-5 text-center">
-                    <div className="col-md-6 col-12 my-md-auto mt-5 mb-0 order-2 order-md-1">
+                    <div className="col-md-6 col-12 row justify-content-md-center my-md-auto mt-5 mb-0 order-2 order-md-1">
                         <h2>Garzeiten</h2>
-                        <div className="mt-4">
+                        <div className="mt-4 col-12 col-md-10">
                             <p>
                                 Die Garzeiten sind bei Straußenfleisch durchschnittlich um ein Drittel
                                 kürzer als bei einem entsprechenden Stück Rindfleisch. In Vakuum
@@ -57,7 +167,7 @@ export default function Straussenfleisch() {
                         </div>
                     </div>
                     <div className="col-md-6 col-12 order-1 order-md-2">
-                        <img src="/images/placeholder/straußenfleisch-filet.jpg" alt={''} className="img-fluid"></img>
+                        <img src="/images/placeholder/straußenfleisch-filet.jpg" alt={''} className="img-fluid w-75"></img>
                     </div>
                 </div>
 
@@ -67,10 +177,7 @@ export default function Straussenfleisch() {
                             Unser Straußenfleischwird frisch & vakuumiert verpackt. Das frische
                             Straußenfleisch wird immer in einer Styroporkiste mit Trockeneis versendet!
                         </p>
-                        <p>Schreibt uns gerne für eine Bestellung über das Kontaktformular an oder schaut im Shop vorbei</p>
-                        <Link href="https://mocfor.com" className="btn btn-outline-primary">
-                          zum Straußenfleisch Shop
-                        </Link>
+                        <p>Schreibt uns gerne für eine Bestellung über das Kontaktformular an.</p>
                     </div>
                     <p>Selbstverständlich bieten wir auch Hundefutter an.</p>
                 </div>
